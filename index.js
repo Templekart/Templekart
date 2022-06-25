6535cea5473d6a0c558228385224c827556bcfec
temple kart <templakaart@gmail.com>
2022-06-25 21:33:29 +0530

    second

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 app.use(express.static(path.join(__dirname, '/public')));
-const PORT = process.env.PORT || 5500;
+const PORT = process.env.PORT || 'https://rajistesting.herokuapp.com/';
 
 app.listen(PORT, console.log('listening to port 5500'));
 
